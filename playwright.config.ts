cadf227be1acf2d6791f09cb64205b9d98249f3d
Nikita Hryhorovych <71128359+HryhorovychN@users.noreply.github.com>
2024-07-22 18:53:42 +0300

    plugin

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
     testDir: './src/tests',
-    timeout: 55000,
+    timeout: 50000,
     use: {
         browserName: 'chromium',
         headless: true,
